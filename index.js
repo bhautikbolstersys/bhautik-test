dacbad05c16034f6d708e4978571e91f54de4f29
BHAUTIK DUDHAT <bhautik.bolstersys@gmail.com>
2026-03-30 12:33:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
 const port = process.env.PORT || 3000;
 const app = express();
 const cors = require('cors');
 
 app.use(cors());
 app.use(express.json());
-
+app.use(morgan('dev'));
 const userRoutes = require('./routes/user.routes');
 app.use('/api/users', userRoutes);
 
